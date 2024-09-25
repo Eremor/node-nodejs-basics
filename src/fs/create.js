a46fcce42557a5d124f74eeb73c0a201d05e2eaf
Eremor <artem.s13@gmail.com>
2024-09-25 15:16:34 +0300

    feat: implement create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import { access, constants, writeFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+  const dirName = path.dirname(fileURLToPath(import.meta.url));
+  const fileName = 'fresh.txt';
+  const pathToFile = path.join(dirName, 'files', fileName);
+  let isExist = false;
+
+  try {
+    await access(pathToFile, constants.R_OK | constants.W_OK)
+      .then(() => isExist = false)
+      .catch(() => isExist = true)
+
+    if (isExist) {
+      writeFile(pathToFile, 'I am fresh and young')
+    } else {
+      throw new Error()
+    }
+  } catch {
+    console.error('FS operation failed')
+  }
 };
 
 await create();
