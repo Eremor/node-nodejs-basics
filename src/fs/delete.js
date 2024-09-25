6b59d7ff7e857025b466637771b1313e8993e7c5
Eremor <artem.s13@gmail.com>
2024-09-25 16:33:37 +0300

    feat: implement delete task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import path from 'path';
+import { access, rm, constants } from 'fs/promises';
+import { fileURLToPath } from 'url';
+
 const remove = async () => {
-    // Write your code here 
+  const dirName = path.dirname(fileURLToPath(import.meta.url));
+  const fileName = 'fileToRemove.txt';
+  const pathToFile = path.join(dirName, 'files', fileName);
+
+  try {
+    await access(pathToFile, constants.F_OK)
+      .catch(() => { throw new Error('FS operation failed')})
+    await rm(pathToFile);
+  } catch (error) {
+    console.error(error.message)
+  }
 };
 
 await remove();
