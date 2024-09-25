25ea4be8f522bd46076a099bb9932046241edb3c
Eremor <artem.s13@gmail.com>
2024-09-25 15:45:00 +0300

    feat: implement copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import path from 'path';
+import { cp } from 'fs/promises';
+import { fileURLToPath } from 'url';
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    const dirName = path.dirname(fileURLToPath(import.meta.url));
+    const pathToOldFolder = path.join(dirName, 'files');
+    const pathToNewFolder = path.join(dirName, 'files_copy');
+
+    await cp(
+      pathToOldFolder,
+      pathToNewFolder,
+      {
+        recursive: true,
+        errorOnExist: true,
+        force: false
+      }
+    ).catch(() => {
+      throw new Error('FS operation failed')
+    })
+  } catch (error) {
+    console.error(error.message)
+  }
 };
 
 await copy();
