1a04b34c9ffb39b2fc40683de2f18ae2eef4d488
Eremor <artem.s13@gmail.com>
2024-09-27 13:59:41 +0300

    feat: implement env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
+import { env } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+  const keys = Object.keys(env).filter((key) => key.startsWith('RSS_'));
+  const result = keys.map((key) => `${key}=${env[key]}`).join('; ');
+  console.log(result)
 };
 
 parseEnv();
