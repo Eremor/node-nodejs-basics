bea28a85042e576d83a9f1114e755f05d7e3e7c0
Eremor <artem.s13@gmail.com>
2024-10-01 11:53:18 +0300

    fix: change arguments

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -2,15 +2,15 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { fork } from 'child_process';
 
-const spawnChildProcess = async (...args) => {
+const spawnChildProcess = async (args) => {
   const dirName = path.dirname(fileURLToPath(import.meta.url));
   const pathToFile = path.join(dirName, 'files', 'script.js');
 
-  const child = fork(pathToFile, [...args], { silent: true });
+  const child = fork(pathToFile, args, { silent: true });
 
   process.stdin.pipe(child.stdin)
   child.stdout.pipe(process.stdout)
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess(1, 2, 3);
+spawnChildProcess([1, 2, 3]);
